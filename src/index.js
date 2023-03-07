8429a3fc546109201ce4cc3a906ea709f7f2f12e
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-03-07 19:05:56 +0530

    Add existing project files to Git

--- src/index.js
+++ src/index.js
@@ -69,30 +69,37 @@ const author = "Taylor Jenkins Reid";
 const title = "Daisy Jones & The Six";
 const img = "./images/book-1.jpg";
 
-// parameters
-const someFunc = (param1, param2) => {
-  console.log(param1, param2);
-};
+// // parameters
+// const someFunc = (param1, param2) => {
+//   console.log(param1, param2);
+// };
 // arguments
-someFunc("job", "developer");
+// someFunc("job", "developer");
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book author={author} title={title} img={img} />
+      <Book author={author} title={title} img={img} />
+      {/* <Book title="random title" number={22} /> */}
+      {/* <Book />
+      <Book /> */}
     </section>
   );
 };
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author} </h4>;{/* <h4>{author.toUpperCase()} </h4>; */}
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author} </h4>
+      {/* <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p> */}
+      {/* <h4>{author.toUpperCase()} </h4>; */}
       {/* <Image />  <Title /> <Author /> */}
+      {/* {console.log(props)}; */}
     </article>
   );
 };
